ce14d7aab09e681d861c3676aac86bfe67bd3834
jung kwang-il <laitdei@gmail.com>
2016-07-18 23:46:55 +0900

    first init

---
+++ src/app/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Library;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"The answer is {new Thing().Get(19, 23)}");
+            for (int i=0; i< 10; ++i) {
+                Console.WriteLine("{0}", i);
+            }
+        }
+    }
+}
---
+++ test/test-library/Tests.cs
@@ -0,0 +1,15 @@
+﻿using Library;
+using Xunit;
+
+namespace TestApp
+{
+    public class LibraryTests
+    {
+        [Fact]
+        public void TestThing() 
+        {
+            Assert.True(true);
+            Assert.Equal(42, new Thing().Get(19, 23));
+        }
+    }
+}
